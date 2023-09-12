c7a38327cc231131fd21ec8e02457eb205d9c601
Shankhadeep Das <shankhascm96@gmail.com>
2023-09-13 01:12:13 +0530

    Modify BrowserRoute with HashRoute

--- src/core/config.ts
+++ src/core/config.ts
@@ -3,5 +3,5 @@ export const Config = {
   PORT: process.env.PORT ? parseInt(process.env.PORT) : 7777,
   REACT_APP_PORT: process.env.REACT_APP_PORT
     ? parseInt(process.env.REACT_APP_PORT)
-    : 3535,
+    : undefined,
 }
